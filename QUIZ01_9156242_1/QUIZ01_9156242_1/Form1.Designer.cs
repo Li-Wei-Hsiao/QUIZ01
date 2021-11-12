78a3d909f9421cb228ee615d4d4ab8f3327cf84d
Li-Wei-Hsiao <80880278+Li-Wei-Hsiao@users.noreply.github.com>
2021-11-13 01:26:44 +0800

    Add files via upload

---
+++ QUIZ01_9156242_1/QUIZ01_9156242_1/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿
+namespace QUIZ01_9156242_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Picture_1 = new System.Windows.Forms.Button();
+            this.Question = new System.Windows.Forms.Label();
+            this.Picture_2 = new System.Windows.Forms.Button();
+            this.Picture_3 = new System.Windows.Forms.Button();
+            this.Picture_4 = new System.Windows.Forms.Button();
+            this.Picture_5 = new System.Windows.Forms.Button();
+            this.Picture_6 = new System.Windows.Forms.Button();
+            this.Picture_7 = new System.Windows.Forms.Button();
+            this.Picture_8 = new System.Windows.Forms.Button();
+            this.Picture_9 = new System.Windows.Forms.Button();
+            this.Answer = new System.Windows.Forms.Label();
+            this.Picture_0 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Picture_1
+            // 
+            this.Picture_1.BackgroundImage = global::QUIZ01_9156242_1.Properties.Resources.a02;
+            this.Picture_1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Picture_1.Location = new System.Drawing.Point(66, 97);
+            this.Picture_1.Name = "Picture_1";
+            this.Picture_1.Size = new System.Drawing.Size(150, 150);
+            this.Picture_1.TabIndex = 0;
+            this.Picture_1.Tag = "1";
+            this.Picture_1.UseVisualStyleBackColor = true;
+            this.Picture_1.Click += new System.EventHandler(this.Picture_Click);
+            // 
+            // Question
+            // 
+            this.Question.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Bold);
+            this.Question.Location = new System.Drawing.Point(36, 20);
+            this.Question.Name = "Question";
+            this.Question.Size = new System.Drawing.Size(677, 50);
+            this.Question.TabIndex = 1;
+            this.Question.Text = "請從下列照片中，找出哪一位是阿笠博士假扮的?";
+            this.Question.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Picture_2
+            // 
+            this.Picture_2.BackgroundImage = global::QUIZ01_9156242_1.Properties.Resources.a03;
+            this.Picture_2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Picture_2.Location = new System.Drawing.Point(66, 263);
+            this.Picture_2.Name = "Picture_2";
+            this.Picture_2.Size = new System.Drawing.Size(150, 150);
+            this.Picture_2.TabIndex = 0;
+            this.Picture_2.Tag = "2";
+            this.Picture_2.UseVisualStyleBackColor = true;
+            this.Picture_2.Click += new System.EventHandler(this.Picture_Click);
+            // 
+            // Picture_3
+            // 
+            this.Picture_3.BackgroundImage = global::QUIZ01_9156242_1.Properties.Resources.a04;
+            this.Picture_3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Picture_3.Location = new System.Drawing.Point(66, 432);
+            this.Picture_3.Name = "Picture_3";
+            this.Picture_3.Size = new System.Drawing.Size(150, 150);
+            this.Picture_3.TabIndex = 0;
+            this.Picture_3.Tag = "3";
+            this.Picture_3.UseVisualStyleBackColor = true;
+            this.Picture_3.Click += new System.EventHandler(this.Picture_Click);
+            // 
+            // Picture_4
+            // 
+            this.Picture_4.BackgroundImage = global::QUIZ01_9156242_1.Properties.Resources.a05;
+            this.Picture_4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Picture_4.Location = new System.Drawing.Point(297, 97);
+            this.Picture_4.Name = "Picture_4";
+            this.Picture_4.Size = new System.Drawing.Size(150, 150);
+            this.Picture_4.TabIndex = 0;
+            this.Picture_4.Tag = "4";
+            this.Picture_4.UseVisualStyleBackColor = true;
+            this.Picture_4.Click += new System.EventHandler(this.Picture_Click);
+            // 
+            // Picture_5
+            // 
+            this.Picture_5.BackgroundImage = global::QUIZ01_9156242_1.Properties.Resources.a06;
+            this.Picture_5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Picture_5.Location = new System.Drawing.Point(297, 263);
+            this.Picture_5.Name = "Picture_5";
+            this.Picture_5.Size = new System.Drawing.Size(150, 150);
+            this.Picture_5.TabIndex = 0;
+            this.Picture_5.Tag = "5";
+            this.Picture_5.UseVisualStyleBackColor = true;
+            this.Picture_5.Click += new System.EventHandler(this.Picture_Click);
+            // 
+            // Picture_6
+            // 
+            this.Picture_6.BackgroundImage = global::QUIZ01_9156242_1.Properties.Resources.a07;
+            this.Picture_6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Picture_6.Location = new System.Drawing.Point(297, 432);
+            this.Picture_6.Name = "Picture_6";
+            this.Picture_6.Size = new System.Drawing.Size(150, 150);
+            this.Picture_6.TabIndex = 0;
+            this.Picture_6.Tag = "6";
+            this.Picture_6.UseVisualStyleBackColor = true;
+            this.Picture_6.Click += new System.EventHandler(this.Picture_Click);
+            // 
+            // Picture_7
+            // 
+            this.Picture_7.BackgroundImage = global::QUIZ01_9156242_1.Properties.Resources.a08;
+            this.Picture_7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Picture_7.Location = new System.Drawing.Point(516, 97);
+            this.Picture_7.Name = "Picture_7";
+            this.Picture_7.Size = new System.Drawing.Size(150, 150);
+            this.Picture_7.TabIndex = 0;
+            this.Picture_7.Tag = "7";
+            this.Picture_7.UseVisualStyleBackColor = true;
+            this.Picture_7.Click += new System.EventHandler(this.Picture_Click);
+            // 
+            // Picture_8
+            // 
+            this.Picture_8.BackgroundImage = global::QUIZ01_9156242_1.Properties.Resources.a09;
+            this.Picture_8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Picture_8.Location = new System.Drawing.Point(516, 263);
+            this.Picture_8.Name = "Picture_8";
+            this.Picture_8.Size = new System.Drawing.Size(150, 150);
+            this.Picture_8.TabIndex = 0;
+            this.Picture_8.Tag = "8";
+            this.Picture_8.UseVisualStyleBackColor = true;
+            this.Picture_8.Click += new System.EventHandler(this.Picture_Click);
+            // 
+            // Picture_9
+            // 
+            this.Picture_9.BackgroundImage = global::QUIZ01_9156242_1.Properties.Resources.a01;
+            this.Picture_9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Picture_9.Location = new System.Drawing.Point(516, 432);
+            this.Picture_9.Name = "Picture_9";
+            this.Picture_9.Size = new System.Drawing.Size(150, 150);
+            this.Picture_9.TabIndex = 0;
+            this.Picture_9.Tag = "Picture_Answer";
+            this.Picture_9.UseVisualStyleBackColor = true;
+            this.Picture_9.Click += new System.EventHandler(this.Picture_answer_Click);
+            // 
+            // Answer
+            // 
+            this.Answer.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Bold);
+            this.Answer.ForeColor = System.Drawing.Color.Red;
+            this.Answer.Location = new System.Drawing.Point(53, 611);
+            this.Answer.Name = "Answer";
+            this.Answer.Size = new System.Drawing.Size(641, 50);
+            this.Answer.TabIndex = 1;
+            this.Answer.Tag = "Answer";
+            this.Answer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Picture_0
+            // 
+            this.Picture_0.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Picture_0.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Picture_0.Enabled = false;
+            this.Picture_0.Location = new System.Drawing.Point(580, 205);
+            this.Picture_0.Name = "Picture_0";
+            this.Picture_0.Size = new System.Drawing.Size(150, 150);
+            this.Picture_0.TabIndex = 0;
+            this.Picture_0.Tag = "1";
+            this.Picture_0.UseVisualStyleBackColor = false;
+            this.Picture_0.Visible = false;
+            this.Picture_0.Click += new System.EventHandler(this.Picture_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 683);
+            this.Controls.Add(this.Answer);
+            this.Controls.Add(this.Question);
+            this.Controls.Add(this.Picture_9);
+            this.Controls.Add(this.Picture_6);
+            this.Controls.Add(this.Picture_3);
+            this.Controls.Add(this.Picture_8);
+            this.Controls.Add(this.Picture_5);
+            this.Controls.Add(this.Picture_2);
+            this.Controls.Add(this.Picture_7);
+            this.Controls.Add(this.Picture_4);
+            this.Controls.Add(this.Picture_0);
+            this.Controls.Add(this.Picture_1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Picture_1;
+        private System.Windows.Forms.Label Question;
+        private System.Windows.Forms.Button Picture_2;
+        private System.Windows.Forms.Button Picture_3;
+        private System.Windows.Forms.Button Picture_4;
+        private System.Windows.Forms.Button Picture_5;
+        private System.Windows.Forms.Button Picture_6;
+        private System.Windows.Forms.Button Picture_7;
+        private System.Windows.Forms.Button Picture_8;
+        private System.Windows.Forms.Button Picture_9;
+        private System.Windows.Forms.Label Answer;
+        private System.Windows.Forms.Button Picture_0;
+    }
+}
+
---
+++ QUIZ01_9156242_1/QUIZ01_9156242_1/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QUIZ01_9156242_1
+{
+    
+    public partial class Form1 : Form
+    {
+        string k = "0";
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Picture_answer_Click(object sender, EventArgs e)
+        {
+            k = "2";
+            show_answer(k);
+        }
+
+        private void Picture_Click(object sender, EventArgs e)
+        {
+            k = "1";
+            show_answer(k);
+            mix_picture(sender);
+        }
+
+        void show_answer(string k)
+        {
+            if (k.Equals("0"))
+            {
+                Answer.Visible = false;
+                Answer.Text = "";
+            }
+            else if (k.Equals("1"))
+            {
+                Answer.Visible = true;
+                Answer.Text = "不對哦，請再看仔細⼀點";
+            }
+            else if (k.Equals("2"))
+            {
+                Answer.Visible = true;
+                Answer.Text = "答對了";
+            }
+        }
+
+        void mix_picture(object sender)
+        {
+            Random random = new Random();
+            Button[] button = {(Button)Picture_0, (Button)Picture_1, (Button)Picture_2, (Button)Picture_3, (Button)Picture_4, (Button)Picture_5, (Button)Picture_6, (Button)Picture_7, (Button)Picture_8, (Button)Picture_9};
+            int number, button_x, button_y;
+
+            for (int i = 1; i < 10; i++)
+            {
+                number = random.Next(1, 10);
+                button[0].Location = button[number].Location;
+                button[number].Location = button[i].Location;
+                button[i].Location = button[0].Location;
+            }
+        }
+    }
+}
